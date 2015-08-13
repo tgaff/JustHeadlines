ba8515f18ce74d518ee99914d1273764d5dfdc31
tgaff <tgaff@alumni.nd.edu>
2015-08-13 16:32:48 -0700

    configure rackup and add a procfile

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 require 'sinatra'
 require 'haml'
 
-set :public_folder, File.dirname(__FILE__) + '/public'
 
-get '/' do
-  query = params['query']
-  if query.nil?
-    erb :index
-  else
-    "not implemented"
+
+class Reader < Sinatra::Base
+
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
+  get '/' do
+    query = params['query']
+    if query.nil?
+      erb :index
+    else
+      "not implemented"
+    end
   end
-
 end
